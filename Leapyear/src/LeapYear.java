1a14d8ceab3b42a9e79ce39ff6305102454adbe7
Frode Naesje <fn@hin.no>
2015-08-20 14:47:52 +0200

    Finished?

--- Leapyear/src/LeapYear.java
+++ Leapyear/src/LeapYear.java
@@ -1,4 +1,6 @@
 
 public class LeapYear {
-
+	public static boolean isLeapyear(int year) {
+		return ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+	}
 }
--- Leapyear/test/LeapYearTest.java
+++ Leapyear/test/LeapYearTest.java
@@ -5,8 +5,22 @@ import org.junit.Test;
 public class LeapYearTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void OddYearShouldAlwaysFail() {
+		assertFalse(LeapYear.isLeapyear(1));
 	}
-
+	
+	@Test
+	public void dividableWith100ShouldNotBeLeapyear() {
+		assertFalse(LeapYear.isLeapyear(100));
+	}
+	
+	@Test
+	public void dividableWith400ShouldBeLeapyear() {
+		assertTrue(LeapYear.isLeapyear(400));
+	}
+	
+	@Test
+	public void dividableWith4ShouldBeLeapyear() {
+		assertTrue(LeapYear.isLeapyear(2016));
+	}
 }

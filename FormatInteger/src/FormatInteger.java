5acb3700d36fe93554e74af42c67367625a388be
Frode Naesje <fn@hin.no>
2015-08-20 13:04:21 +0200

    Initial checkin of FormatInteger

---
+++ FormatInteger/src/FormatInteger.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+// Exercise 6.37
+public class FormatInteger {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter an integer: ");
+    int number = input.nextInt();
+    System.out.print("Enter the width: ");
+    int width = input.nextInt();
+    
+    System.out.println("The formatted number is " + format(number, width));
+  }
+
+  public static String format(int number, int width) {
+    String result = number + "";
+    if (number < 0)
+    	return result;
+    
+    for (int i = 0; i < width - len(number); i++)
+      result = "0" + result;
+      
+    return result;
+  }
+
+  public static int len(int number) {
+    int size = 0;
+    while (number > 0) {
+      size += 1;
+      number = number / 10;
+    }
+    
+    return size;
+  }
+}
---
+++ FormatInteger/test/FormatIntegerTest.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.matchers.JUnitMatchers.*;
+import org.junit.*;
+
+public class FormatIntegerTest {
+	
+	@Test
+	public void zeroWidthShouldReturnNoLeadingZeros() {
+		assertEquals("1",FormatInteger.format(1,0));
+		//assertThat(FormatInteger.format(1,0),is(equalTo("1")));
+		assertEquals("999",FormatInteger.format(999,0));
+		//assertThat(FormatInteger.format(999,0),is(equalTo("999")));
+	}
+	
+	@Test
+	public void negativeNumbersShouldReturnNoLeadingZeroes() {
+		assertEquals("-1",FormatInteger.format(-1,5));
+		assertEquals("-999",FormatInteger.format(-999,5));
+	}
+	
+	@Test
+	public void sunnyDayScenarios() {
+		assertEquals("01",FormatInteger.format(1,2));
+		assertEquals("00999",FormatInteger.format(999,5));
+	}
+	@Test
+	public void maxIntWithoutWidthShouldReturnNoLeadingZeros() {
+		// Arrange
+		String stringWithMAX_VALUE = "" + Integer.MAX_VALUE;
+		// Act + Assert
+		assertEquals(stringWithMAX_VALUE,FormatInteger.format(Integer.MAX_VALUE,0));
+		// assertThat(FormatInteger.format(Integer.MAX_VALUE,0), is(equalTo(stringWithMAX_VALUE)));
+    }
+	@Test
+	
+	public void maxIntWithWidthShouldReturnLeadingZeros() {
+		// Arrange
+		String stringWithMAX_VALUE = "" + Integer.MAX_VALUE;
+		// Act + Assert
+		assertEquals("00"+stringWithMAX_VALUE,FormatInteger.format(Integer.MAX_VALUE,stringWithMAX_VALUE.length()+2));
+		// assertThat(FormatInteger.format(Integer.MAX_VALUE,stringWithMAX_VALUE.length()+2), is(equalTo("00"+stringWithMAX_VALUE)));
+    }
+}

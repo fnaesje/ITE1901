580fd6263310b7f42e8176212001719538bbc48d
Frode Naesje <fn@hin.no>
2016-08-29 12:56:39 +0200

    Laga constructorer

--- Counter/src/Counter.java
+++ Counter/src/Counter.java
@@ -1,8 +1,18 @@
 public class Counter {
-	int count = 0;
+	int count;
+	
+	public Counter() {
+		count = 0;
+	}
+	public Counter(int seed) {
+		count = seed;
+	}
 
 	public int increment() {
-		return count += 1;
+		if (count < Integer.MAX_VALUE)
+		  count += 1;
+		return count;
+		
 	}
 
 	public int decrement() {
--- Counter/test/CounterTest.java
+++ Counter/test/CounterTest.java
@@ -1,23 +1,31 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import static org.hamcrest.CoreMatchers.*;
 
 public class CounterTest {
     Counter counter1;
 
     @Before
-    void setUp() {
+    public void setUp() {
         counter1 = new Counter();
     } 
 
     @Test
     public void testIncrement() {
-        assertTrue(counter1.increment() == 1);
-        assertTrue(counter1.increment() == 2);
+        assertTrue(counter1.increment() == 1);        
+        assertThat(counter1.increment(), is(2)); // demo Hamcrest
      }
 
     @Test
     public void testDecrement() {
         assertTrue(counter1.decrement() == -1);
+        assertEquals(-2, counter1.decrement());
+    }
+    @Test
+    public void testMax() {
+        counter1 = new Counter(Integer.MAX_VALUE);
+    	assertTrue(counter1.increment() == Integer.MAX_VALUE);
+       
     }
 }
 
